98609419c2a719df79d33a1301be1cfcec6fe768
congzw <46074987@qq.com>
2017-09-11 17:30:31 +0800

    add RenderSectionOrPartial

---
+++ src/UnifyDemo/Helpers/RazorPageHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace UnifyDemo.Helpers
+{
+    public static class RazorPageHelper
+    {
+        public static IHtmlString RenderSectionOrPartial<TModel>(this WebViewPage<TModel> page, string name)
+        {
+            var renderSection = page.RenderSection(name, false);
+            if (renderSection != null)
+            {
+                return renderSection;
+            }
+            return page.Html.Partial(name);
+        }
+    }
+}
