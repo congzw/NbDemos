c7a7979c05224eb8c553d526ee9caab84c466c01
congzw <46074987@qq.com>
2017-09-11 18:01:50 +0800

    add HideSection and RenderSectionWithPartial

--- src/UnifyDemo/Helpers/RazorPageHelper.cs
+++ src/UnifyDemo/Helpers/RazorPageHelper.cs
@@ -15,5 +15,45 @@ namespace UnifyDemo.Helpers
             }
             return page.Html.Partial(name);
         }
+        
+        /// <summary>
+        /// 用指定的部分页渲染@section
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="page"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static IHtmlString RenderSectionWithPartial<TModel>(this WebViewPage<TModel> page, string name)
+        {
+            //@section _Xxx
+            //{
+            //    @Html.Partial("_Xxx")
+            //}
+            page.DefineSection(name, () =>
+            {
+                page.Html.RenderPartial(name);
+            });
+            return null;
+        }
+
+        /// <summary>
+        /// 隐藏指定的@section
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="page"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static IHtmlString HideSection<TModel>(this WebViewPage<TModel> page, string name)
+        {
+            //@section _Xxx
+            //{
+            //
+            //}
+            page.DefineSection(name, () =>
+            {
+                
+            });
+            return null;
+        }
     }
 }
