12fd4886472236ae9b38f2c95f32299b425b058e
congzw <46074987@qq.com>
2017-09-11 13:48:41 +0800

    add helper demos

---
+++ src/UnifyDemo/Helpers/StringExtensions.cs
@@ -0,0 +1,43 @@
+namespace UnifyDemo.Helpers
+{
+    /// <summary>
+    /// Provides utility methods for converting string values to other data types.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        ///     Removes dashes ("-") from the given object value represented as a string and returns an empty string ("")
+        ///     when the instance type could not be represented as a string.
+        ///     <para>
+        ///         Note: This will return the type name of given isntance if the runtime type of the given isntance is not a
+        ///         string!
+        ///     </para>
+        /// </summary>
+        /// <param name="value">The object instance to undash when represented as its string value.</param>
+        /// <returns></returns>
+        public static string UnDash(this object value)
+        {
+            return ((value as string) ?? string.Empty).UnDash();
+        }
+
+        /// <summary>
+        ///     Removes dashes ("-") from the given string value.
+        /// </summary>
+        /// <param name="value">The string value that optionally contains dashes.</param>
+        /// <returns></returns>
+        public static string UnDash(this string value)
+        {
+            return (value ?? string.Empty).Replace("-", string.Empty);
+        }
+
+        /// <summary>
+        /// 指示指定的字符串是 null、空还是仅由空白字符组成。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
