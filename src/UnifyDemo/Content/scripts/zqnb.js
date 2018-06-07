82c45f239a16139b11d396a043defe250a52e500
congzw <2981810300@qq.com>
2018-06-07 14:32:00 +0800

    temp save

--- src/UnifyDemo/Content/scripts/zqnb.js
+++ src/UnifyDemo/Content/scripts/zqnb.js
@@ -13,6 +13,14 @@
                 && typeof str === 'string'
                 && this.toUpperCase() === str.toUpperCase());
     }
+    Array.prototype.contains = function (element) {
+        for (var i = 0; i < this.length; i++) {
+            if (this[i] === element) {
+                return true;
+            }
+        }
+        return false;
+    };
 
     zqnb.inited = true;
     console.log('>> zqnb inited');
