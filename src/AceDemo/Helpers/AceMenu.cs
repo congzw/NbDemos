0ce3435f35f59dce6a46c6f129e24e57b76f7347
congzw <2981810300@qq.com>
2018-10-09 14:53:49 +0800

    refactor ace nav

---
+++ src/AceDemo/Helpers/AceMenu.cs
@@ -0,0 +1,14 @@
+﻿//namespace AceDemo.Helpers
+//{
+//    public class AceMenu
+//    {
+//        public string Id { get; set; }
+//        public string ParentId { get; set; }
+//        public string Text { get; set; }
+//        public string Href { get; set; }
+
+
+//        public bool active { get; set; }
+//        public bool open { get; set; }
+//    }
+//}
