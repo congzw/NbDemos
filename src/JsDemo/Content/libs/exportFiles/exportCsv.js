de82c5517a962dfaf852cc46ad23e192cd3cc88b
congzw <2981810300@qq.com>
2019-02-25 16:10:20 +0800

    add exportCsv demo

---
+++ src/JsDemo/Content/libs/exportFiles/exportCsv.js
@@ -0,0 +1,70 @@
+﻿(function (name, definition) {
+    if (typeof module !== 'undefined') {
+        module.exports = definition();
+    }
+    else if (typeof define === 'function' && typeof define.amd === 'object') {
+        define(definition);
+    } else {
+        this[name] = definition();
+    }
+}('exportCsv', function () {
+    "use strict";
+
+    function convertArrayOfObjectsToCSV(args) {
+        var result, ctr, keys, columnDelimiter, lineDelimiter, data;
+
+        data = args.data || null;
+        if (data == null || !data.length) {
+            return null;
+        }
+
+        columnDelimiter = args.columnDelimiter || ',';
+        lineDelimiter = args.lineDelimiter || '\n';
+
+        keys = Object.keys(data[0]);
+
+        result = '';
+        result += keys.join(columnDelimiter);
+        result += lineDelimiter;
+
+        data.forEach(function (item) {
+            ctr = 0;
+            keys.forEach(function (key) {
+                if (ctr > 0) {
+                    result += columnDelimiter;
+                }
+
+                result += item[key];
+                ctr++;
+            });
+            result += lineDelimiter;
+        });
+
+        return result;
+    }
+
+    function downloadCSV(config) {
+        var data, filename, link;
+
+        var csv = convertArrayOfObjectsToCSV({
+            data: config.data || ""
+        });
+        if (csv == null) return;
+
+        filename = config.filename || 'export.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    }
+
+    return {
+        downloadCSV: downloadCSV
+    };
+}));
